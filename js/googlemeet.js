4ae75f21406091d06df732b31a849acff08eed54
Abcent1k <snakyizze@gmail.com>
2024-02-05 02:45:37 +0200

    timer function now works

--- js/googlemeet.js
+++ js/googlemeet.js
@@ -102,21 +102,22 @@ function timerControl() {
     let hoursAndMinutes = threshold.split(':');
     let hours = parseInt(hoursAndMinutes[0]);
     let minutes = parseInt(hoursAndMinutes[1]);
+    let seconds = parseInt(hoursAndMinutes[2]);
 
-    let threshold_time = (hours * 60 * 60 + minutes * 60) * 1000;
+    let threshold_time_ms = (hours * 60 * 60 + minutes * 60 + seconds) * 1000;
 
     chrome.runtime.sendMessage({
         action: 'set_timer',
         tab_id: tab_id,
         tab_url: tab_url,
-        time: threshold_time / 1000
+        time: threshold_time_ms / 1000
     });
 
     timerId = setTimeout(() => {
         console.log("Threshold met. User will now leave the google meet");
 
         endCall();
-    }, threshold_time)
+    }, threshold_time_ms)
 }
 
 
